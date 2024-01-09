23ff22e086b26dfde276f53e9b7070b105d5eb1f
tiffanyriley <147551447+tiffanyriley@users.noreply.github.com>
2024-01-09 07:09:55 -0500

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,22 @@
+import React, {useState, useEffect} from 'react';
 import "./App.css";
 
 function App() {
+
+  const [quote, setQuote] = useState("");
+  const [author, setauthor] = useState("");
   return (
     <div className="App">
+      <div className="app-container">
+        <h1>Quotable Quote Generator</h1>
+      </div>
       <div className="quote">
         <h2>New Quote</h2>
         <small>-Author-</small>
-      </div>
+      </div><br />
       <button className="button">Fetch New Quote</button>
     </div>
+
   );
 }
 
