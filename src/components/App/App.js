74f230e6fd4901558d89b9e0646f22a814c13f9e
tiffanyriley <147551447+tiffanyriley@users.noreply.github.com>
2024-01-09 08:12:29 -0500

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,20 +4,32 @@ import "./App.css";
 function App() {
 
   const [quote, setQuote] = useState("");
-  const [author, setauthor] = useState("");
-  return (
-    <div className="App">
-      <div className="app-container">
-        <h1>Quotable Quote Generator</h1>
-      </div>
-      <div className="quote">
-        <h2>New Quote</h2>
-        <small>-Author-</small>
-      </div><br />
-      <button className="button">Fetch New Quote</button>
-    </div>
+  const [author, setAuthor] = useState("");
 
-  );
-}
+  useEffect (() => {
+    fetch("http://api.quotable.io/random")
+      .then(res => res.json())
+      .then(
+        (quote) => {
+          // setQuote();
+          // setAuthor();
+          console.log()
+        }
+      )
+  },[]);
+  // const fetchNewQuote = async () => {
+  //   try {
+  //     const response = await fetch('https://api-ninjas.com/api/quotes');
+  //     const data = await response.json();
 
-export default App;
+  //     setQuote(data.quote);
+  //     setAuthor(data.author);
+  //   } catch (error){
+  //     console.error('Error fetching new quote:', error);
+  //   }
+  // };
+  
+  // useEffect(() => {
+    
+  // }
+  // )
